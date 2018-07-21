923cdb0f9ba54f88dd2cd530b959e77f13dc2958
neilkg <neilkg@umich.edu>
2018-07-21 16:22:19 -0400

    lower_bound and upper_bound

--- algorithm.h
+++ algorithm.h
@@ -2,6 +2,7 @@
 #define ALGORITHM_H
 
 #include <utility>
+#include "vector.h"
 
 // NEIL GOLLAPUDI
 
@@ -13,12 +14,47 @@ void swap(T& one, T& two) {
 }
 
 // must be a forward iterator
-template <class Iterator, class T>
-constexpr Iterator lower_bound(Iterator begin, Iterator end, const T& val);
+template <class Iterator, class T, class Compare>
+constexpr Iterator lower_bound(Iterator first, Iterator last, const T& val, Compare comp) {
+    Iterator it;
+    typename std::iterator_traits<Iterator>::difference_type count, step;
+    count = std::distance(first, last);
+    
+    while (count > 0) {
+        it = first;
+        step = count / 2;
+        std::advance(it, step);
+        if (comp(*it, val)) {
+            first = ++it;
+            count -= step + 1;
+        }
+        else {
+            count = step;
+        }
+    }
+    return first;
+}
 
 // must be a forward iterator
-template <class Iterator, class T>
-constexpr Iterator uppper_bound(Iterator begin, Iterator end, const T& val);
+template <class Iterator, class T, class Compare>
+constexpr Iterator uppper_bound(Iterator first, Iterator last, const T& val, Compare comp) {
+    Iterator it;
+    typename std::iterator_traits<Iterator>::difference_type count, step;
+    count = std::distance(first,last);
+    while (count > 0) {
+        it = first;
+        step=count / 2;
+        std::advance(it,step);
+        if (!comp(val, *it)) {
+            first = ++it;
+            count -= step + 1;
+        }
+        else {
+            count = step;
+        }
+    }
+    return first;
+}
 
 
 
